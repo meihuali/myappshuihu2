18452c1376125c9faf8a9444f1ee2e45c218e295
meihuali <77299007@qq.com>
2017-10-24 15:27:52 +0800

    /加入了一个图片

--- app/src/main/java/com/example/anonymous/greendao/helper/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/anonymous/greendao/helper/MySQLiteOpenHelper.java
@@ -9,7 +9,7 @@ import com.example.anonymous.greendao.gen.DaoMaster;
 
 public class MySQLiteOpenHelper extends DaoMaster.OpenHelper {
     public MySQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory) {
-        super(context, name, factory);
+        super(context, name, factory); //
     }
 
     @Override
